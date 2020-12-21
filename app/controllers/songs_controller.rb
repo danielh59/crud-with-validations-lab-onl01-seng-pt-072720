bc48808b0d062ed4d7d41fa7818788a11c3e221d
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-12-21 07:17:27 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,53 +1,51 @@
 class SongsController < ApplicationController
-  def index
-      @songs = Song.all
-  end
+    before_action :set_song!, only: [:show, :edit, :update, :destroy]
 
-  def show
-    @song = Song.find(params[:id])
-  end
-
-  def new
-    @song = Song.new
-  end
-
-  def create
-       @song = Song.new(song_params)
-
-    if @song.save
-
-       redirect_to song_path(@song)
-     else
-       render :new
+    def new
+        @song = Song.new
     end
-  end
+
+    def create
+        @song = Song.new(song_params)
+        if @song.save
+            redirect_to song_path(@song)
+        else
+            render "new"
+        end
+    end
+
+    def index
+        @songs = Song.all
+    end
+
+    def show
+    end
 
     def edit
-      @song = Song.find(params[:id])
     end
 
     def update
-      @song = Song.find(params[:id])
-      @song.update(song_params)
-
-      if @song.valid?
-
-          redirect_to song_path(@song)
+        if @song.update(song_params)
+            redirect_to song_path(@song)
         else
-
-        render :edit
-      end
+            render "edit"
+        end
     end
 
-    def delete
-      @song = song.find(params[:id])
-      @song.destroy
+    def destroy
+        @song.destroy
+        redirect_to songs_path
     end
 
+    private
 
-  private
+    def set_song!
+        @song = Song.find(params[:id])
+    end
 
-  def song_params
-    params.permit(:title, :released, :release_year, :artist_name, :genre)
-  end
+    def song_params
+        params.require(:song).permit(
+            :title, :released, :release_year, :artist_name, :genre
+            )
+    end
 end
