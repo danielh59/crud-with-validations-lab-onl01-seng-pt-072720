80bbfa9aa98c917508d3b7bf7aa65e277b63e77a
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-12-21 07:21:53 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,5 @@
 class Song < ApplicationRecord
-  validates :title, presence: true
+  validates :title, uniqueness: true, presence: true
   validates :released, inclusion: { in: [ true, false ] }
   validates :release_year, presence: true, if: -> { released == true}
   validate :release_year_valid?
